f0d295f8721bba5a10119fc5bf26cf961f0a2464
Cristy <Cristy@DESKTOP-IC173HR>
2018-04-19 13:27:24 -0500

    -Análisis Léxico -Agrega las variables a una lista -Detecta palabras reservadas -Análisis Sintáctico

---
+++ src/analisislexico/Sintactico.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisislexico;
+
+/**
+ *
+ * @author Cristy
+ */
+public class Sintactico {
+    
+    public void Programa(){
+        
+    }
+    
+    public void Var(){
+        
+    }
+    
+    public void AuxVar(){
+        
+    }
+    
+    public void Fijo(){
+        
+    }
+    
+    public void AuxFijo(){
+        
+    }
+    
+    public void AuxFijo2(){
+        
+    }
+    
+    public void Instrucciones(){
+        
+    }
+    
+    public void AuxFor(){
+        
+    }
+    
+    public void AuxIf(){
+        
+    }
+    
+    public void Condicion(){
+        
+    }
+    
+    public void AuxCond2(){
+        
+    }
+    
+    public void AuxCond3(){
+        
+    }
+    
+    public void AuxCond4(){
+        
+    }
+    
+    public void Expresion(){
+        
+    }
+    
+    public void AuxExpre(){
+        
+    }
+    
+    public void AuxExpre2(){
+        
+    }
+    
+}
